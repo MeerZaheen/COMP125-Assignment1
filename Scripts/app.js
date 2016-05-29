d85c0428f14684da1837540d8b2a80041fe22762
meerzaheen_90 <meerzaheen_90@hotmail.com>
2016-05-29 17:39:31 -0400

    working on project.html

--- Scripts/app.js
+++ Scripts/app.js
@@ -15,13 +15,23 @@
     paragraphElements[0] = document.getElementById("paragraphOne")
     paragraphElements[1] = document.getElementById("paragraphTwo")
     paragraphElements[2] = document.getElementById("paragraphThree")
+    paragraphElements[3] = document.getElementById("paragraphTwo.1")
 
     // define your paragraphs array (best way and most common)
     var paragraphs = [];
     
     // data for my pages
+    
+    // index intro paragraph
     paragraphs[0] = "I am Meer Zaheen, born on March 12, 1990. I have been living in Toronto, Canada for almost 18 years. I graduated from Seneca College as a Civil Engineer in 2013. Since then, I was employed at an engineering firm called CCI-Group. Formerly I was at a 9 month contract with EXP Inc. I was always passionate about evolving products and disruptive technologies and hence why I chose to continue my education at Centennial College as a Software Engineer. Outside of Engineering and technology, I like to play competitive video games, absorb different cultures and enjoy amazing food. I have always had a passion for sports and personal fitness – not only to lead a healthy lifestyle but to bring out my competitive spirit. I live to serve my talents as an engineer, artist and a fitness enthusiast. I create balance in work, play, and community. Also, I want to be the kind of person my cat already thinks I am."
-    paragraphs[1] = " On this page, you will be able to see some of my previous and on-going projects!"
+    
+    // project intro paragraph
+    paragraphs[1] = "On this page, you will be able to see some of my previous and on-going projects!"
+    
+    // project 1 paragraph (PC)
+    paragraphs[3] = "This was my first ever custom built desktop PC."
+    
+    // contact me paragraph
     paragraphs[2] = " I'm a Civil Engineer graduate from Seneca College, Newnham Campus." 
     + " I worked professionally as a Civil Engineer for 2 years and realized that I would like to pursue a different career." + " I'm currently enrolled in Centennial College as a Software Engineer."
     + " I enjoy First Person Shooter video games." + " I'm also a PC hardware enthusiast, as I have build my own PC - more information would be found on My Projects page."
